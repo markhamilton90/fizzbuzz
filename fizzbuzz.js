384b227ebb7d825fbf8977492d059131b9ef336b
markhamilton90 <msh387@gmail.com>
2015-07-13 18:03:15 -0500

    working fizzbuzz app

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,40 @@
 
 $('document').ready( function() {
 
-	for (var i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 == 0) {
-			document.write("<p>FizzBuzz</p>");
-		}
-		else if (i % 3 == 0) {
-			document.write("<p>Fizz</p>");
-		}
-		else if (i % 5 == 0) {
-			document.write("<p>Buzz</p>");
-		}
-		else
-			document.write("<p> " + i + " </p>");
+	function FizzBuzz(number) {
+
+		var msg = "";
+		var fizzNumber = parseInt(number) || 0;
+
+		if (fizzNumber > 0 && fizzNumber % 1 === 0) {
+			for (var i = 1; i <= fizzNumber; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					msg += "<p>FizzBuzz</p>";
+				}
+				else if (i % 3 == 0) {
+					msg += "<p>Fizz</p>";
+				}
+				else if (i % 5 == 0) {
+					msg += "<p>Buzz</p>";
+				}
+				else {
+					msg += "<p>" + i + "</p>";
+				}
+
+			document.getElementById('output').innerHTML = msg;
+
+			} 
+
+		} else {
+				alert("You must enter an integer whole number!");
+			}
 	}
+
+	document.getElementById('user-input').addEventListener('keyup', function(e) {
+		if (e.which === 13) {
+			if (this.value !== "") {
+				FizzBuzz(this.value);
+			}
+		}
+	});
 });
